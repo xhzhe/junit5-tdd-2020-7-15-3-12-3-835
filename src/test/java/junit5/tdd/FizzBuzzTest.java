73a10b5e7efda948ce9024efbb38a4ae728abe97
alex <alex.h.z.xu@oocl.com>
2020-07-23 11:35:51 +0800

    [alex xu] feature: add should_return_fizz_when_play_fizz_buzz_given_3 test

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -15,4 +15,15 @@ public class FizzBuzzTest {
         //then
         Assertions.assertEquals("1", res);
     }
+
+    @Test
+    public void should_return_fizz_when_play_fizz_buzz_given_3() {
+        //given
+        int number = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String res = fizzBuzz.play(number);
+        //then
+        Assertions.assertEquals("fizz", res);
+    }
 }
