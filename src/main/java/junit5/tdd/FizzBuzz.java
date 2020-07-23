49d38c6ad0157ea8cc6cb3a89fc4dab02685af60
alex <alex.h.z.xu@oocl.com>
2020-07-23 11:40:45 +0800

    [alex xu] feature: refactor implement of play() and add new feature

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -1,7 +1,16 @@
 package junit5.tdd;
 
+import com.google.common.base.Strings;
+
 public class FizzBuzz {
     public String play(int number) {
-        return String.valueOf(number);
+        String res = "";
+        if (number % 3 == 0) {
+            res += "fizz";
+        }
+        if (Strings.isNullOrEmpty(res)) {
+            res = String.valueOf(number);
+        }
+        return res;
     }
 }
